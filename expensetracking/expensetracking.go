5792af2b548a5fdccb7ab2bc09c81b5a9282941a
smalldog124 <panumars124@gmail.com>
2018-09-22 16:49:21 +0700

    [Edited]:แยกการรวมราคาระหว่างวันเป็นฟังก์ชันcalculateTotalPrice

--- expensetracking/expensetracking.go
+++ expensetracking/expensetracking.go
@@ -11,12 +11,7 @@ func ExpenseAVGpreDay(orders string) float64 {
 	var totalPrice int
 	for _, value := range listOrder {
 		orderDay := split(value, " ")
-
-		for index := 1; index <= len(orderDay)-1; index++ {
-			temp := orderDay[index]
-			priceString := temp[1:]
-			totalPrice = sum(priceString, totalPrice)
-		}
+		totalPrice += calculateTotalPrice(orderDay)
 	}
 	avg := float64(totalPrice) / float64(len(listOrder))
 	return math.Round(avg*100) / 100
@@ -25,7 +20,15 @@ func ExpenseAVGpreDay(orders string) float64 {
 func split(dataString, sysbol string) []string {
 	return strings.Split(dataString, sysbol)
 }
-
+func calculateTotalPrice(orderDay []string) int {
+	var totalPricePerDay int
+	for index := 1; index <= len(orderDay)-1; index++ {
+		temp := orderDay[index]
+		priceString := temp[1:]
+		totalPricePerDay = sum(priceString, totalPricePerDay)
+	}
+	return totalPricePerDay
+}
 func sum(priceString string, totalPrice int) int {
 	price, _ := strconv.Atoi(priceString)
 	return price + totalPrice
